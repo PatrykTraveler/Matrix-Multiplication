4cd1def5a3fb4278f91e7af7fb4b6a67746dcec7
Traveler <patryktraveler@gmail.com>
2018-06-01 22:13:57 +0200

    Added labels to plot

--- plot.jl
+++ plot.jl
@@ -6,7 +6,7 @@ y3 = readdlm("./Results/matmul.txt")
 y4 = readdlm("./Results/cache.txt")
 y5 = readdlm("./Results/dotcache.txt")
 
-p = plot(x, y1, label = "No optimization", xscale = :log10, yscale = :log10)
+p = plot(x, y1, xlabel = "Size", ylabel = "Time", label = "No optimization", xscale = :log10, yscale = :log10)
 plot!(p, x, y2, label = "Using dot product")
 plot!(p, x, y3, label = "Using matmul")
 plot!(p, x, y4, label = "Using chunks")
